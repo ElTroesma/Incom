bb65cf55c039ef5c100a5a5c87f7f5fe6998d062
Juan Garcia <Juan.Garcia070699@outlook.es>
2018-06-20 17:09:13 -0300

    Avance en la página

---
+++ assets/js/modal.js
@@ -0,0 +1,7 @@
+function showModal() {
+	document.getElementById('contact-modal').style.display = 'block';
+}
+
+function hideModal() {
+	document.getElementById('contact-modal').style.display = 'none';
+}
