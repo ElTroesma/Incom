6864b8a51e3f87da59ded1cc4caaae421e578c1a
Juan Garcia <Juan.Garcia070699@outlook.es>
2018-06-25 04:12:29 -0300

    Último avance. Finalización de la página
    Finalización del sitio web. Posiblemes errores por solucionar. Hace
    falta testeo

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,3 +5,13 @@ function showModal() {
 function hideModal() {
 	document.getElementById('contact-modal').style.display = 'none';
 }
+
+/* Menu Modal */
+
+function showMenu() {
+	document.getElementById('menu-modal').style.display = 'block';
+}
+
+function hideMenu() {
+	document.getElementById('menu-modal').style.display = 'none';
+}
